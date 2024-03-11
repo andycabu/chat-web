4999546f07376b9fa6724e6274272e211d4879cf
andycabu <andycabu@gmail.com>
2024-03-11 23:55:29 +0100

    añadimos nueva base de datos

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const FROM_PHONE_NUMBER_ID = import.meta.env.VITE_FROM_PHONE_NUMBER_ID;
 export const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
 export const URL_WHAT = `https://graph.facebook.com/v19.0/${FROM_PHONE_NUMBER_ID}/messages`;
-export const URL = "http://localhost:3000/";
+export const URL = "http://localhost:3000/api/";
